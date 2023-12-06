68d3d80d1ac0dba6557264c8e7dc7e7a799d8cb2
jeromerh <119083415+jeromerh@users.noreply.github.com>
2023-12-06 16:41:18 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,3 @@
-nose_X=0
-nose_y=0
-
 diference=0
 
 right_wrist_x=0
@@ -24,5 +21,16 @@ function modelLoad(){
 function gotPoses(results){
 if(results.length>0){
 console.log(results)
+left_wrist_x=results[0].pose.leftWrist.x
+right_wrist_x=results[0].pose.rightWrist.x
+diference=floor(left_wrist_x-right_wrist_x)
 }
+}
+
+function draw(){
+    background("ligthgreen")
+    document.getElementById("tamaño_cuadrado").innerHTML="el ancho y alto del cuadrado sera:"+diference
+    fill("red")
+    textSize(diference)
+    text("que mira bobo que mira bobo anda anda pa ya bobo anda pa ya",50,400)
 }
